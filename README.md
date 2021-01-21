ecfe52503f69be831b4c9ac909ea32fb75efd37f
Sebastien Dorgan <sebastien.dorgan@csgroup.eu>
2021-01-21 21:59:15 +0100

    better developement environment

--- README.md
+++ README.md
@@ -1,1 +1,34 @@
-# eopf
+# EOPF Proof of Concept
+
+This project is a Proof of Concept to demonstrate the capabilities of the EOPF framework developed by the consortium led by CS GROUP France.
+
+## Development environment
+
+This projects contains a batteries included full web development environment using [code-server](https://github.com/cdr/code-server) and [JupyterLab](https://jupyterlab.readthedocs.io/en/stable/).
+
+### Prerequisites
+
+To start the development environment install first:
+- [docker](https://docs.docker.com/engine/install/)
+- [docker-compose](https://docs.docker.com/compose/install/)
+
+### Start and stop environment
+
+Running the environment is straightforward, simply type:
+
+```bash
+docker-compose up
+```
+
+The first run builds the docker image, depending on your network connection speed, this stage can take some time.
+
+Once done, your environment is ready, you can access it on http://localhost/ui/
+
+*Note: the environment do not use HTTPS, modern browser enforcing security automatically add https:// do not miss the http:// par of the URL*
+
+To stop the environment type:
+
+```
+docker-compose stop
+```
+
--- docker/start.sh
+++ docker/start.sh
@@ -2,9 +2,11 @@
 function start_services {
     systemctl enable codeserver.service
     systemctl enable jupyterlab.service
+    systemctl enable ray.service
 
     systemctl start codeserver.service
     systemctl start jupyterlab.service
+    systemctl start ray.service
 }
 
 
@@ -18,13 +20,15 @@ function rename_user {
     id $1
     sed -i "s/USER/${USERNAME}/g" /etc/systemd/system/jupyterlab.service
     sed -i "s/USER/${USERNAME}/g" /etc/systemd/system/codeserver.service
+    sed -i "s/USER/${USERNAME}/g" /etc/systemd/system/ray.service
     echo "export USER=${USERNAME}" > /home/${USERNAME}/.local/user_env.sh
     echo "export USERFULLNAME=${USERFULLNAME}" >> /home/${USERNAME}/.local/user_env.sh
     echo "export PYTHONPATH=${PYTHONPATH}" >> /home/${USERNAME}/.local/user_env.sh
     echo "export AWS_ACCESS_KEY_ID=${AWS_ACCESS_KEY_ID}" >> /home/${USERNAME}/.local/user_env.sh
     echo "export AWS_SECRET_ACCESS_KEY=${AWS_SECRET_ACCESS_KEY}" >> /home/${USERNAME}/.local/user_env.sh
     echo "export AWS_DEFAULT_REGION=${AWS_DEFAULT_REGION}" >> /home/${USERNAME}/.local/user_env.sh
-    echo "export PATH=/home/${USERNAME}/.local/bin:\${PATH}" >> /home/${USERNAME}/.local/user_env.sh
+    echo "export PATH=/home/${USERNAME}/.local/bin:/usr/local/devtools/bin:\${PATH}" >> /home/${USERNAME}/.local/user_env.sh
+    echo "conda activate eopf" >> /home/${USERNAME}/.local/user_env.sh
     echo "source /home/${USERNAME}/.local/user_env.sh" >> /home/${USERNAME}/.bashrc
 }
 
